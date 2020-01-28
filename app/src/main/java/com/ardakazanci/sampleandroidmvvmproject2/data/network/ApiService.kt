b352eff2a1f23b86d7807a0dfb2c7aba0c2a6d37
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-01-28 21:10:15 +0300

    Moshi Implement

--- app/src/main/java/com/ardakazanci/sampleandroidmvvmproject2/data/network/ApiService.kt
+++ app/src/main/java/com/ardakazanci/sampleandroidmvvmproject2/data/network/ApiService.kt
@@ -1,14 +1,20 @@
 package com.ardakazanci.sampleandroidmvvmproject2.data.network
 
-import retrofit2.Call
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.http.GET
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 
 private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
